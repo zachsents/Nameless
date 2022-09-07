710706eb50013eee37e08a1341e3446f5acb0c75
Zach Sents <zachsents@gmail.com>
2022-09-06 22:15:49 -0600

    Added module alias during development

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default ({ mode }) => defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            ...(mode == "development" && { "nameless-graph-exec": path.resolve(__dirname, "../execution") })
+        }
+    }
 })
